5a88a2eb8ed262aeb43bcc06f0d1d5c3c913585a
Yatharth Nagpal <nagpalyatharth99@gmail.com>
2025-03-15 23:56:14 +0530

    Upload all files of the website

--- HEALTH++/navigate.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("openButtonsPage").addEventListener("click", function() {
-        window.location.href = "buttons.html";
-    });
-});
